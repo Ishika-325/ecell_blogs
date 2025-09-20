33acdb85d50e8e1b6e6384487f7f79886ef04e82
Kratika6101 <kratikarajput243@gmail.com>
2025-09-20 21:50:16 +0530

    added header and bloghero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import BlogPage from './blog_detail'
+import BlogPage from './Blog_detail'
+import BlogHero from './BlogHero'
+import Header from './Header'
 
 
 function App() {
   return (
     <div className='main'>
+      <Header/>
+      <BlogHero/>
       <BlogPage />     
     </div>
   )
